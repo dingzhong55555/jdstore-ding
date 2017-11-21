43718332ab0604d610d1a81801e2176d04316a52
dingzhong <dingzhong55555@gmail.com>
2017-11-21 22:58:44 +0800

    install font-awesome-rails

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,3 @@
-module FlashesHelper
-  FLASH_CLASSES = { alert: "danger", notice: "success", warning: "warning"}.freeze
+module ApplicationHelper
 
-  def flash_class(key)
-    FLASH_CLASSES.fetch key.to_sym, key
-  end
-
-  def user_facing_flashes
-    flash.to_hash.slice "alert", "notice","warning"
-  end
 end
