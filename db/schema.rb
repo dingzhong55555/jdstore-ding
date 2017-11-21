c4db7595f530ec7c8783b400cf508548cf901ddb
dingzhong <dingzhong55555@gmail.com>
2017-11-21 23:52:11 +0800

    implement product show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171121151940) do
+ActiveRecord::Schema.define(version: 20171121153035) do
 
   create_table "products", force: :cascade do |t|
     t.string   "title"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20171121151940) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.boolean  "is_admin",    default: false
+    t.string   "image"
   end
 
   create_table "users", force: :cascade do |t|
