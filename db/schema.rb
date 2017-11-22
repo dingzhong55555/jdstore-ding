4f5f93d76accd3761089e68f4df6d32c20d999e4
dingzhong <dingzhong55555@gmail.com>
2017-11-22 23:56:55 +0800

    add token to order

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171122154654) do
+ActiveRecord::Schema.define(version: 20171122155227) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer  "cart_id"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20171122154654) do
     t.string   "shipping_address"
     t.datetime "created_at",                   null: false
     t.datetime "updated_at",                   null: false
+    t.string   "token"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
