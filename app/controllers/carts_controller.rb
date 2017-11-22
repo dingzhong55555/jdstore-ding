525c092bd88591bc037c683583c4a2c954ff4a9f
dingzhong <dingzhong55555@gmail.com>
2017-11-22 22:01:03 +0800

    add delete product function

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -8,4 +8,11 @@ class CartsController < ApplicationController
     redirect_to :back
     flash[:notice] = "购物车已经清空"
   end
+
+  def destroy
+    @cart_item = current_cart.cart_items.find_by_product_id(params[:id])
+    @cart_item.destroy
+    redirect_to :back
+    flash[:warnig] = "已经删除#{@cart_item.product.title}"
+  end
 end
