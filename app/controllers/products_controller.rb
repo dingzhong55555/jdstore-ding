3aa0a662523aec22e39b7ec9ca3843fbb5c7fa78
dingzhong <dingzhong55555@gmail.com>
2017-11-22 21:28:38 +0800

    add cart function

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,4 +7,11 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    current_cart.add_product_to_cart(@product)
+    redirect_to products_path
+    flash[:notice] = "加入购物车"
+  end
 end
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -31,7 +31,7 @@ class ImageUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
 
-  process resize_to_fit: [800, 800]
+  process resize_to_fill: [800, 800]
 
   version :thumb do
     process resize_to_fit: [200, 200]
